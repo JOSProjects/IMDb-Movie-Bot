9eb1208bfa342aeeaad6e8d348ccbb6243a0a8b2
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-11-28 22:50:03 +0530

    Update url_shortner.py

--- plugins/josprojects/url_shortner.py
+++ plugins/josprojects/url_shortner.py
@@ -190,7 +190,7 @@ async def short(link):
     
     # Send the text
     try:
-        shorten_urls += "\n\nmade by @josprojects | jospsupport 🔥"
+        shorten_urls += "\n\nmade by @josprojects | @jospsupport 🔥"
         return shorten_urls
     except Exception as error:
         return error
