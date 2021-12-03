d56e1e5f8d04efefa69e69516f568bed74a9d68f
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-12-03 10:59:13 +0530

    Update script.py

--- script.py
+++ script.py
@@ -277,9 +277,8 @@ Music download modules, for those who love music.
 • /song or /mp3 (songname) - download song from yt servers.
 • /video or /mp4 (songname) - download video from yt servers.
 
-<b>YouTube Thumnail Download</b>
+<b>YouTube Thumbnail Download</b>
 • /ytthumb (youtube link)
-
 <b>Example:</b> <code>/ytthumb https://youtu.be/h6PtzFYaMxQ</code>
 
 <b>NOTE:</b>
