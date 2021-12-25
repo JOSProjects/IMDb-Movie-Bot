24dc3797f263a7b9079a68307860718cffe1a959
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-12-25 21:51:59 +0530

    Mistake 🤭😂

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -873,7 +873,7 @@ async def auto_filter(client, msg, spoll=False):
             await message.delete()
         except Exception as e:
             logger.exception(e)
-            fek = await message.reply_photo(photo="https://telegra.ph/file/82b5bbbab6d5e5593b6b2.jpg", caption=cap,r eply_to_message_id=reply_id, reply_markup=InlineKeyboardMarkup(btn))
+            fek = await message.reply_photo(photo="https://telegra.ph/file/82b5bbbab6d5e5593b6b2.jpg", caption=cap, reply_to_message_id=reply_id, reply_markup=InlineKeyboardMarkup(btn))
             await asyncio.sleep(600)
             await fek.delete()
             await msg.delete()
