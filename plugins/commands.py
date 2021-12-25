9f0db2a128b4b937e78482d55b054bdfa000ba97
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-12-25 21:15:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -116,16 +116,26 @@ async def start(client, message):
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
         try:
-            f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+            f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
         except Exception as e:
             logger.exception(e)
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
+    buttons = [
+        [
+            InlineKeyboardButton('support', url='https://t.me/JOSPSupport'),
+            InlineKeyboardButton('channel', url='https://t.me/JosProjects')
+        ],
+        [
+            InlineKeyboardButton('Series & Movie Club', url=f'https://t.me/+y53tWFUw6Q43NzE9')
+        ]
+        ]
     await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup(buttons)
         )
                     
 
