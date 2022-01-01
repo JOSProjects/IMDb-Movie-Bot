620cde212f6eb3ef1bc54d8bfbc6d9f0fb1105c1
ASHIK MHD <85797232+Ashik231@users.noreply.github.com>
2022-01-01 20:31:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/josprojects/tgmoviebot.git /tgmoviebot
+git clone -b master https://github.com/Ashik231/tgmoviebot.git /tgmoviebot
 cd /tgmoviebot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
