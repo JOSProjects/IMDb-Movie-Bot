fb67c74655529f150fad237a4f66001ba7c6410b
ASHIK MHD <85797232+Ashik231@users.noreply.github.com>
2022-01-01 23:26:11 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -161,8 +161,8 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
             ],
             [
                 InlineKeyboardButton(
-                    text=f"Projects Channel",
-                    url="https://t.me/josprojects"
+                    text=f"Owner",
+                    url="https://t.me/Unavailable4allTime"
                 )
             ],
         ]
