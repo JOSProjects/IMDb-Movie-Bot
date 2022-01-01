4bea429cb4d293503f58e8a3a98d642f945c46f6
ASHIK MHD <85797232+Ashik231@users.noreply.github.com>
2022-01-01 20:33:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Ashik231/tgmoviebot.git /tgmoviebot
-cd /tgmoviebot
+git clone -b master https://github.com/Ashik231/ngc_muthmani.git /ngc_muthmani
+cd /ngc_muthmani
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
