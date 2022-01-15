80d8a4d8e7b8eb140836ed61c7085c76ad9eca8e
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-01-15 15:56:08 +0530

    fix parsing captions

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -17,12 +17,12 @@ logger.setLevel(logging.INFO)
 async def gen_link_s(bot, message):
     replied = message.reply_to_message
     if not replied:
-        return await message.reply('Reply to a message to get a Shareable Link.')
+        return await message.reply('Reply to a message to get a shareable link.')
     file_type = replied.media
     if file_type not in ["video", 'audio', 'document']:
         return await message.reply("Reply to a supported media")
     file_id, ref = unpack_new_file_id((getattr(replied, file_type)).file_id)
-    await message.reply(f"<b>Here is your Link:</b>\n\n<code>https://t.me/{temp.U_NAME}?start={file_id}</code>")
+    await message.reply(f"Here is your Link:\nhttps://t.me/{temp.U_NAME}?start={file_id}")
     
     
 @Client.on_message(filters.command('batch') & filters.user(ADMINS))
@@ -65,39 +65,16 @@ async def gen_link_batch(bot, message):
     if chat_id in FILE_STORE_CHANNEL:
         string = f"{f_msg_id}_{l_msg_id}_{chat_id}"
         b_64 = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-        return await sts.edit(f"<b>Here is your Link</b>\n\n<code>https://t.me/{temp.U_NAME}?start=DSTORE-{b_64}</code>")
-
-    msgs_list = []
-    c_msg = f_msg_id
-    
-    diff = l_msg_id - f_msg_id
-    
+        return await sts.edit(f"Here is your link https://t.me/{temp.U_NAME}?start=DSTORE-{b_64}")
+ 
     FRMT = "Generating Link...\nTotal Messages: `{total}`\nDone: `{current}`\nRemaining: `{rem}`\nStatus: `{sts}`"
-    if diff <= 200:
-        msgs = await bot.get_messages(f_chat_id, list(range(f_msg_id, l_msg_id+1)))
-        msgs_list += msgs
-    else:
-        c_msg = f_msg_id
-        while True:
-            new_diff = l_msg_id - c_msg
-            if new_diff > 200:
-                new_diff = 200
-            elif new_diff <= 0:
-                break
-            msgs = await bot.get_messages(f_chat_id, list(range(c_msg, c_msg+new_diff)))
-            msgs_list += msgs
-            try:
-                await sts.edit(FRMT.format(total=diff, current=(c_msg - f_msg_id), rem=(l_msg_id - c_msg), sts="Fetching Messages"))
-            except:
-                pass
-            c_msg += new_diff
 
     outlist = []
     
     # file store without db channel
     og_msg = 0
     tot = 0
-    for msg in msgs_list:
+    async for msg in bot.iter_messages(f_chat_id, l_msg_id, f_msg_id):
         tot += 1
         if msg.empty or msg.service:
             continue
@@ -107,10 +84,13 @@ async def gen_link_batch(bot, message):
         try:
             file_type = msg.media
             file = getattr(msg, file_type)
+            caption = getattr(msg, 'caption', '')
+            if caption:
+                caption = caption.html
             if file:
                 file = {
                     "file_id": file.file_id,
-                    "caption": msg.caption,
+                    "caption": caption,
                     "title": getattr(file, "file_name", ""),
                     "size": file.file_size,
                 }
@@ -120,7 +100,7 @@ async def gen_link_batch(bot, message):
             pass
         if not og_msg % 20:
             try:
-                await sts.edit(FRMT.format(total=diff, current=tot, rem=(diff - tot), sts="Saving Messages"))
+                await sts.edit(FRMT.format(total=l_msg_id-f_msg_id, current=tot, rem=((l_msg_id-f_msg_id) - tot), sts="Saving Messages"))
             except:
                 pass
     with open(f"batchmode_{message.from_user.id}.json", "w+") as out:
@@ -128,4 +108,4 @@ async def gen_link_batch(bot, message):
     post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore.")
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
-    await sts.edit(f"<b>Here is your link</b>\nContains `{og_msg}` files.\n <code>https://t.me/{temp.U_NAME}?start=BATCH-{file_id}</code>")
+    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{file_id}")
