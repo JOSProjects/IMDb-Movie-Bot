9ed2704a2d4b92fdff3242f957d3877156a51b7a
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-01-20 19:39:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -259,7 +259,19 @@ async def start(client, message):
         file_id=file_id,
         caption=f_caption,
         protect_content=True if pre == 'filep' else False,
-        reply_markup=InlineKeyboardMarkup[[InlineKeyboardButton('support', url='https://t.me/JOSPSupport'),InlineKeyboardButton('channel', url='https://t.me/JosProjects')],[InlineKeyboardButton('Series & Movie Club', url=f'https://t.me/+y53tWFUw6Q43NzE9')]]
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        'support', url='https://t.me/JOSPSupport'),
+                    InlineKeyboardButton(
+                        'channel', url='https://t.me/JosProjects')
+                ],
+                [
+                    InlineKeyboardButton(
+                        'Series & Movie Club', url=f'https://t.me/+y53tWFUw6Q43NzE9')
+                ]
+            ]
         )
                     
 
