1d7e00592c6409b2f988a5092a7bb9f35ae0fa3b
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-01-20 18:40:09 +0530

    file store for all

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -94,10 +94,7 @@ async def count_filters(group_id):
     mycol = mydb[str(group_id)]
 
     count = mycol.count()
-    if count == 0:
-        return False
-    else:
-        return count
+    return False if count == 0 else count
 
 
 async def filter_stats():
@@ -115,3 +112,4 @@ async def filter_stats():
     totalcollections = len(collections)
 
     return totalcollections, totalcount
+
