482fcad15f129e61b763f8ee95cda7f48ccbf856
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-01-20 19:34:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -254,21 +254,12 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
-    buttons = [
-        [
-            InlineKeyboardButton('support', url='https://t.me/JOSPSupport'),
-            InlineKeyboardButton('channel', url='https://t.me/JosProjects')
-        ],
-        [
-            InlineKeyboardButton('Series & Movie Club', url=f'https://t.me/+y53tWFUw6Q43NzE9')
-        ]
-        ]
     await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
         protect_content=True if pre == 'filep' else False,
-        reply_markup=InlineKeyboardMarkup(buttons)
+        reply_markup=InlineKeyboardMarkup[[InlineKeyboardButton('support', url='https://t.me/JOSPSupport'),InlineKeyboardButton('channel', url='https://t.me/JosProjects')],[InlineKeyboardButton('Series & Movie Club', url=f'https://t.me/+y53tWFUw6Q43NzE9')]]
         )
                     
 
