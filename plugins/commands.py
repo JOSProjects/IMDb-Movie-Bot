cfb52745eaf06b866f8c7221725e72da41254704
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-01-20 19:26:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -144,15 +144,15 @@ async def start(client, message):
                     f_caption=f_caption
             if f_caption is None:
                 f_caption = f"{title}"
-           buttons = [
-               [
-                   InlineKeyboardButton('support', url='https://t.me/JOSPSupport'),
+            buttons = [
+                [
+                    InlineKeyboardButton('support', url='https://t.me/JOSPSupport'),
                    InlineKeyboardButton('channel', url='https://t.me/JosProjects')
-               ],
-               [
-                   InlineKeyboardButton('Series & Movie Club', url=f'https://t.me/+y53tWFUw6Q43NzE9')
-               ]
-               ]
+                ],
+                [
+                    InlineKeyboardButton('Series & Movie Club', url=f'https://t.me/+y53tWFUw6Q43NzE9')
+                ]
+                ]
             try:
                 await client.send_cached_media(
                     chat_id=message.from_user.id,
