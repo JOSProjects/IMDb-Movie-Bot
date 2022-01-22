6ed386e0c8ef8cb90ed370385ec5190e1e6155b6
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-01-22 10:51:49 +0530

    Fixed

--- plugins/josprojects/share_text.py
+++ plugins/josprojects/share_text.py
@@ -8,20 +8,27 @@ def share_link(text: str) -> str:
 
 @Client.on_message(filters.command(["share", "sharetext", "st", "stxt", "shtxt", "shtext"]))
 async def groupmsg(client, message):
-    k = await message.reply_text("**Making...**")
     reply = message.reply_to_message
-    if not reply:
-        await k.edit("`Reply any Message`")
-        return
     input_split = message.text.split(None, 1)
     if len(input_split) == 2:
         input_text = input_split[1]
     elif reply and (reply.text or reply.caption):
         input_text = reply.text or reply.caption
     else:
-        await k.edit(
-            f"""Something wrong Ask @{SUPPORT_CHAT}""",
+        await message.reply_text(
+            text=f"Something went wrong 😑😑,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Support Chat", url=f"https://t.me/{SUPPORT_CHAT}")
+                    ]                
+                ]
+            ),
+            reply_to_message_id=message.message_id
+        )
         )
         return
-    await k.edit(share_link(input_text))
+    await message.reply_text(share_link(input_text))
+
         
