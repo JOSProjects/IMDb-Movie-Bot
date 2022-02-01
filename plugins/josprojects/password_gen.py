2947aa214ea8b683810c0439f78da6518dfe1415
NG ROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2022-02-01 15:34:31 +0400

    Update password_gen.py

--- plugins/josprojects/password_gen.py
+++ plugins/josprojects/password_gen.py
@@ -31,6 +31,6 @@ async def password(bot, update):
         text = "Sorry... Failed To Create Password, Because Limit is 1 to 100."
     else:
         random_value = "".join(random.sample(password, limit))
-        text = f"**Limit :-** `{str(limit)}`.\n**Password :-** `{random_value}`**\n\nJoin @JOSPSupport",
-        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('join projects channel', url='https://telegram.me/josprojects')]])
+        text = f"**Limit :-** `{str(limit)}`.\n**Password :-** `{random_value}`**\n\nJoin @adarshmedias",
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('join movies channel', url='https://telegram.me/mallurockersclub')]])
     await message.edit_text(text, True)
