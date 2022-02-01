4c2ac08fcb11e9f951365e955e782d273a474657
NG ROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2022-02-01 15:38:27 +0400

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -7,7 +7,7 @@ from utils import broadcast_messages
 import asyncio
         
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/JosProjects
+# https://telegram.me/mallurockersclub
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
