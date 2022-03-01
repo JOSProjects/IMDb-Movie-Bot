4bcdda6b3c98f66bde8d0a1ffa8809a8c854e907
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-03-01 18:06:14 +0530

    Repo Name

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/josprojects/tgmoviebot.git /tgmoviebot
-cd /tgmoviebot
+git clone -b master https://github.com/JOSProjects/IMDb-Movie-Bot.git /IMDb-Movie-Bot
+cd /IMDb-Movie-Bot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
