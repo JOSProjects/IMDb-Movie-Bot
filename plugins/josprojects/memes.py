5eb060c6ee88b1a0dca0e5299250da381b1b417b
Romeybrar12 <106407653+Romeybrar12@users.noreply.github.com>
2022-06-07 00:37:33 +0530

    Update memes.py

--- plugins/josprojects/memes.py
+++ plugins/josprojects/memes.py
@@ -21,7 +21,7 @@ async def aesthetic(client, message):
     text = "".join(aesthetify(text))
     await status_message.edit(text)
 
-# DART------------ https://telegram.me/Josprojects ------------ #
+# DART------------ https://telegram.me/moviehtv ------------ #
 
 # EMOJI CONSTANTS
 DART_E_MOJI = "🎯"
@@ -43,7 +43,7 @@ async def throw_dart(client, message):
         reply_to_message_id=rep_mesg_id
     )
 
-# DICE------------ https://telegram.me/Josprojects ------------ #
+# DICE------------ https://telegram.me/moviehtv ------------ #
 
 # EMOJI CONSTANTS
 DICE_E_MOJI = "🎲"
@@ -65,7 +65,7 @@ async def roll_dice(client, message):
         reply_to_message_id=rep_mesg_id
     )
 
-# LUCK------------ https://telegram.me/Josprojects ------------ #
+# LUCK------------ https://telegram.me/moviehtv ------------ #
 
 # EMOJI CONSTANTS
 TRY_YOUR_LUCK = "🎰"
@@ -86,7 +86,7 @@ async def luck_cownd(client, message):
         reply_to_message_id=rep_mesg_id
     )
 
-# GOAL------------ https://telegram.me/Josprojects ------------ #
+# GOAL------------ https://telegram.me/moviehtv ------------ #
 
 # EMOJI CONSTANTS
 GOAL_E_MOJI = "⚽"
@@ -107,7 +107,7 @@ async def roll_dice(client, message):
         reply_to_message_id=rep_mesg_id
     )
 
-# RUNS------------ https://telegram.me/Josprojects ------------ #
+# RUNS------------ https://telegram.me/moviehtv ------------ #
 
 import random
 
