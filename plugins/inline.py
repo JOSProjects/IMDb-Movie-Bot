3da0aa1c89de8d2435f377e2a1e4dc63371c0330
ᴀꜱʜɪᴋ ᴍʜᴅ <85797232+Ashik231@users.noreply.github.com>
2022-07-07 16:33:41 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,25 +1,34 @@
 import logging
-from typing import Union
-from typing import Optional, List
-import pyrogram
-from struct import pack
-from pyrogram import raw
-from pyrogram.file_id import FileId, FileType, PHOTO_TYPES, DOCUMENT_TYPES
-from pyrogram import raw, types, utils
 from pyrogram import Client, emoji, filters
 from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
 from database.ia_filterdb import get_search_results
-from utils import is_subscribed, get_size
+from utils import is_subscribed, get_size, temp
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
 
+async def inline_users(query: InlineQuery):
+    if AUTH_USERS:
+        if query.from_user and query.from_user.id in AUTH_USERS:
+            return True
+        else:
+            return False
+    if query.from_user and query.from_user.id not in temp.BANNED_USERS:
+        return True
+    return False
 
-@Client.on_inline_query(filters.user(AUTH_USERS) if AUTH_USERS else None)
+@Client.on_inline_query()
 async def answer(bot, query):
     """Show search results for given inline query"""
+    
+    if not await inline_users(query):
+        await query.answer(results=[],
+                           cache_time=0,
+                           switch_pm_text='okDa',
+                           switch_pm_parameter="hehe")
+        return
 
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
@@ -79,10 +88,6 @@ async def answer(bot, query):
             pass
         except Exception as e:
             logging.exception(str(e))
-            await query.answer(results=[], is_personal=True,
-                           cache_time=cache_time,
-                           switch_pm_text=str(e)[:63],
-                           switch_pm_parameter="error")
     else:
         switch_pm_text = f'{emoji.CROSS_MARK} No results'
         if string:
@@ -104,166 +109,3 @@ def get_reply_markup(query):
     return InlineKeyboardMarkup(buttons)
 
 
-class InlineQueryResultCachedDocument(types.InlineQueryResult):
-    """Link to a file stored on the Telegram servers.
-    
-    By default, this file will be sent by the user with an optional caption. 
-    Alternatively, you can use input_message_content to send a message with the specified content instead of the file.
-    
-    Parameters:
-        title (``str``):
-            Title for the result.
-        
-        file_id (``str``):
-            Pass a file_id as string to send a media that exists on the Telegram servers.
-            
-        id (``str``, *optional*):
-            Unique identifier for this result, 1-64 bytes.
-            Defaults to a randomly generated UUID4.
-            
-        description (``str``, *optional*):
-            Short description of the result.
-            
-        caption (``str``, *optional*):
-            Caption of the document to be sent, 0-1024 characters.
-        
-        parse_mode (``str``, *optional*):
-            By default, texts are parsed using both Markdown and HTML styles.
-            You can combine both syntaxes together.
-            Pass "markdown" or "md" to enable Markdown-style parsing only.
-            Pass "html" to enable HTML-style parsing only.
-            Pass None to completely disable style parsing.
-
-        caption_entities (List of :obj:`~pyrogram.types.MessageEntity`):
-                List of special entities that appear in the caption, which can be specified instead of *parse_mode*.
-            
-        reply_markup (:obj:`~pyrogram.types.InlineKeyboardMarkup`, *optional*):
-            Inline keyboard attached to the message.
-            
-        input_message_content (:obj:`~pyrogram.types.InputMessageContent`):
-            Content of the message to be sent.
-    """
-
-    def __init__(
-        self,
-        title: str,
-        file_id: str,
-        id: str = None,
-        description: str = None,
-        caption: str = "",
-        parse_mode: Optional[str] = object,
-        caption_entities: List["types.MessageEntity"] = None,
-        reply_markup: "types.InlineKeyboardMarkup" = None,
-        input_message_content: "types.InputMessageContent" = None
-    ):
-        super().__init__("file", id, input_message_content, reply_markup)
-
-        self.file_id = file_id
-        self.title = title
-        self.description = description
-        self.caption = caption
-        self.caption_entities = caption_entities
-        self.parse_mode = parse_mode
-        self.reply_markup = reply_markup
-        self.input_message_content = input_message_content
-
-    async def write(self, client: "pyrogram.Client"):
-        document = get_input_file_from_file_id(self.file_id)
-
-        message, entities = (
-            await utils.parse_text_entities(
-                client,
-                self.caption,
-                self.parse_mode,
-                self.caption_entities,
-            )
-        ).values()
-
-        return raw.types.InputBotInlineResultDocument(
-            id=self.id,
-            type=self.type,
-            title=self.title,
-            description=self.description,
-            document=document,
-            send_message=(
-                await self.input_message_content.write(client, self.reply_markup)
-                if self.input_message_content else raw.types.InputBotInlineMessageMediaAuto(
-                    reply_markup=await self.reply_markup.write(client) if self.reply_markup else None,
-                    message=message,
-                    entities=entities
-                )
-            )
-        )
-
-def get_input_file_from_file_id(
-    file_id: str,
-    expected_file_type: FileType = None,
-) -> Union["raw.types.InputPhoto", "raw.types.InputDocument"]:
-    try:
-        decoded = FileId.decode(file_id)
-    except Exception:
-        raise ValueError(
-            f'Failed to decode "{file_id}". The value does not represent an existing local file, '
-            f'HTTP URL, or valid file id.'
-        )
-
-    file_type = decoded.file_type
-
-    if expected_file_type is not None and file_type != expected_file_type:
-        raise ValueError(f'Expected: "{expected_file_type}", got "{file_type}" file_id instead')
-
-    if file_type in (FileType.THUMBNAIL, FileType.CHAT_PHOTO):
-        raise ValueError(f"This file_id can only be used for download: {file_id}")
-
-    if file_type in PHOTO_TYPES:
-        return raw.types.InputPhoto(
-            id=decoded.media_id,
-            access_hash=decoded.access_hash,
-            file_reference=decoded.file_reference,
-        )
-
-    if file_type in DOCUMENT_TYPES:
-        return raw.types.InputDocument(
-            id=decoded.media_id,
-            access_hash=decoded.access_hash,
-            file_reference=decoded.file_reference,
-        )
-
-    raise ValueError(f"Unknown file id: {file_id}")
-
-
-def encode_file_id(s: bytes) -> str:
-    r = b""
-    n = 0
-
-    for i in s + bytes([22]) + bytes([4]):
-        if i == 0:
-            n += 1
-        else:
-            if n:
-                r += b"\x00" + bytes([n])
-                n = 0
-
-            r += bytes([i])
-
-    return base64.urlsafe_b64encode(r).decode().rstrip("=")
-
-
-def encode_file_ref(file_ref: bytes) -> str:
-    return base64.urlsafe_b64encode(file_ref).decode().rstrip("=")
-
-
-def unpack_new_file_id(new_file_id):
-    """Return file_id, file_ref"""
-    decoded = FileId.decode(new_file_id)
-    file_id = encode_file_id(
-        pack(
-            "<iiqq",
-            int(decoded.file_type),
-            decoded.dc_id,
-            decoded.media_id,
-            decoded.access_hash,
-        )
-    )
-    file_ref = encode_file_ref(decoded.file_reference)
-    return file_id, file_ref
