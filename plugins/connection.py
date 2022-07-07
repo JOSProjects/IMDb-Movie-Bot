b0cedcc3f5e4bd09e38d54556f57be37bae0396e
ᴀꜱʜɪᴋ ᴍʜᴅ <85797232+Ashik231@users.noreply.github.com>
2022-07-07 16:24:11 +0530

    Update connection.py

--- plugins/connection.py
+++ plugins/connection.py
@@ -3,11 +3,13 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.connections_mdb import add_connection, all_connections, if_active, delete_connection
 from info import ADMINS
 import logging
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
+
 @Client.on_message((filters.private | filters.group) & filters.command('connect'))
-async def addconnection(client,message):
+async def addconnection(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
         return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
@@ -31,9 +33,9 @@ async def addconnection(client,message):
     try:
         st = await client.get_chat_member(group_id, userid)
         if (
-            st.status != "administrator"
-            and st.status != "creator"
-            and str(userid) not in ADMINS
+                st.status != "administrator"
+                and st.status != "creator"
+                and userid not in ADMINS
         ):
             await message.reply_text("You should be an admin in Given group!", quote=True)
             return
@@ -54,7 +56,7 @@ async def addconnection(client,message):
             addcon = await add_connection(str(group_id), str(userid))
             if addcon:
                 await message.reply_text(
-                    f"Sucessfully connected to **{title}**\nNow manage your group from my pm !",
+                    f"Successfully connected to **{title}**\nNow manage your group from my pm !",
                     quote=True,
                     parse_mode="md"
                 )
@@ -73,12 +75,12 @@ async def addconnection(client,message):
             await message.reply_text("Add me as an admin in group", quote=True)
     except Exception as e:
         logger.exception(e)
-        await message.reply_text('Some error occured! Try again later.', quote=True)
+        await message.reply_text('Some error occurred! Try again later.', quote=True)
         return
 
 
 @Client.on_message((filters.private | filters.group) & filters.command('disconnect'))
-async def deleteconnection(client,message):
+async def deleteconnection(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
         return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
@@ -92,9 +94,9 @@ async def deleteconnection(client,message):
 
         st = await client.get_chat_member(group_id, userid)
         if (
-            st.status != "administrator"
-            and st.status != "creator"
-            and str(userid) not in ADMINS
+                st.status != "administrator"
+                and st.status != "creator"
+                and str(userid) not in ADMINS
         ):
             return
 
@@ -105,9 +107,8 @@ async def deleteconnection(client,message):
             await message.reply_text("This chat isn't connected to me!\nDo /connect to connect.", quote=True)
 
 
-
 @Client.on_message(filters.private & filters.command(["connections"]))
-async def connections(client,message):
+async def connections(client, message):
     userid = message.from_user.id
 
     groupids = await all_connections(str(userid))
