0fbe2e952c3285848aaaaf96b9b9c7919938e775
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-11-26 20:39:06 +0530

    Okda

--- script.py
+++ script.py
@@ -1,3 +1,15 @@
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
 class Script(object):
     START_TXT = """Hello {},
 
