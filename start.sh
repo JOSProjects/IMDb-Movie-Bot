b1bc15e274c99d4cea2bf58e6fad00d2a5a2ce9e
rajraswin51 <129712623+rajraswin51@users.noreply.github.com>
2023-05-18 20:25:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/JOSProjects/IMDb-Movie-Bot.git /IMDb-Movie-Bot
+git clone -b master https://github.com/rajraswin51/IMDb-Movie-Bot.git /IMDb-Movie-Bot
 cd /IMDb-Movie-Bot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
